4e44d9256860970d1f772f238ae54d0a01754b11
Juan Di Nella <juan.dinella@gmail.com>
2020-04-24 20:41:23 -0300

    :tada:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,25 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+let URL;
+if (process.env.NODE_ENV === 'production') {
+  URL = "https://cactusauria.netlify.com/";
+} else {
+  URL = 'http://localhost:8000';
+}
+
 module.exports = {
+  siteMetadata: {
+    title: `Cactu Ecommerce <3`,
+    description: `La pagina de Cactu <3`,
+    author: `juandinella.com`,
+    siteUrl: URL,
+  },
   plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-resolve-src",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -10,5 +27,19 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Playfair Display\:900`, `Montserrat\:400,700`],
+        display: "swap",
+      },
+    },
   ],
-}
+};
